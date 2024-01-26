46509810bcc530c2af69fe771f98d5b3b1805b1d
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-01-26 10:42:21 +0100

    Routing et creation du footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <div>Kasa</div>
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Header from './components/Header'
+import About from './pages/About'
+import FicheLogement from './pages/FicheLogement'
+import Error404 from './pages/404'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/fiche/:logementID" element={<FicheLogement />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
