9086cd3160573cbd4294ead9d5cb8c490d8efc8a
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-01-26 15:53:36 +0100

    Update responsive layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import logo from "../../assets/LOGO.svg"
 import "./styles.scss"
 
 function Header() {
+  const location = useLocation()
+  const isCurrentPage = (path) => location.pathname === path
   return (
     <header className="header">
       <Link to="/">
         <img className="header__logo" src={logo} alt="logo Kasa" />
       </Link>
       <nav className="nav">
-        <Link className="nav__link" to="/">
+        <Link
+          className={`nav__link ${
+            isCurrentPage("/") ? "nav__link--currentpage" : ""
+          }`}
+          to="/"
+        >
           Accueil
         </Link>
-        <Link className="nav__link" to="/about">
+        <Link
+          className={`nav__link ${
+            isCurrentPage("/about") ? "nav__link--currentpage" : ""
+          }`}
+          to="/about"
+        >
           A Propos
         </Link>
       </nav>
