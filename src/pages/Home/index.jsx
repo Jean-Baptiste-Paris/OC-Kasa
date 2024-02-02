7ff0478a8c62b89627eefef641387104da4b66d5
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-02 19:53:36 +0100

    Update responsive design of global layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import Banner from "../../components/Banner"
 import bannerImage from "../../assets/images/banner-image-home.jpg"
 
 function Home() {
-  const bannerMessage = "Chez vous, partout et ailleurs"
+  const bannerMessage = "Chez vous, partout\u00A0et\u00A0ailleurs"
   return (
     <div className="Home">
       <Banner bannerImage={bannerImage} bannerMessage={bannerMessage} />
