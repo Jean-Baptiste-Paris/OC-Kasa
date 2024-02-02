1e37abf8953cac5f1cc6caef86c4a9e878a93099
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-02 18:19:44 +0100

    Réorganisation des fichiers Déplacement dans assets images dans src/assets/images Deplacement du composant KasaRouter dans src/components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "../../assets/LOGO-white.svg"
+import logo from "../../assets/images/LOGO-white.svg"
 import "./styles.scss"
 
 function Footer() {
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom"
-import logo from "../../assets/LOGO.svg"
+import logo from "../../assets/images/LOGO.svg"
 import "./styles.scss"
 
 function Header() {
