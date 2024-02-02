64c66025dea1ed54984bca3065f91c72861205ba
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-02 23:08:05 +0100

    Update Grid component Navigation vers les fiche logement depuis les Thumbnail

--- src/components/Grid/Thumbnail.jsx
+++ src/components/Grid/Thumbnail.jsx
@@ -1,11 +1,11 @@
-function Thumbnail({ title, cover }) {
+function Thumbnail({ id, title, cover }) {
   const thumbStyle = cover && {
     backgroundImage: `url(${cover})`,
   }
   return (
-    <div className="gallery-item" style={thumbStyle}>
+    <a className="gallery-item" style={thumbStyle} href={`/logement/${id}`}>
       <p className="gallery-item__text">{title}</p>
-    </div>
+    </a>
   )
 }
 
--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -5,7 +5,7 @@ function Grid({ logements }) {
   return (
     <div className="gallery">
       {logements.map(({ id, title, cover }) => (
-        <Thumbnail key={id} title={title} cover={cover} />
+        <Thumbnail key={id} id={id} title={title} cover={cover} />
       ))}
     </div>
   )
