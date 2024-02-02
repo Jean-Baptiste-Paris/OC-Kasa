23a50c3c8071221c93ee566da7e3f4f12384371e
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-02 23:04:06 +0100

    Update FicheLogement to verify ID on render Redirection vers la page d'erreur en cas d'ID inexistant

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,9 +1,16 @@
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import logements from "../../assets/data/logements.json"
+import { useEffect } from "react"
 
 function FicheLogement() {
   const { logementID } = useParams()
   const logement = logements.find((element) => element.id === logementID)
+
+  useEffect(() => {
+    if (!logement) {
+      return <Navigate to="/404" replace={true} />
+    }
+  }, [logement])
   return (
     <div className="Logement page">
       <h1>{logement.title}</h1>
