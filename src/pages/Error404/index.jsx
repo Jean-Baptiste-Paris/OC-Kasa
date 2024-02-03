95e81b5de78750b20edb5ca5ce038e43d00a2658
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-03 13:24:36 +0100

    Update responsive of Error404 page

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+
 function Error404() {
   return (
-    <div className="404 page">
-      <h1>404</h1>
+    <div className="Error404 page">
+      <p className="Error404__404">404</p>
+      <h1 className="Error404__title">
+        Oups! La page que vous demandez n'existe pas.
+      </h1>
+      <Link className="Error404__link" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   )
 }
