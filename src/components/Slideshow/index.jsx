c21a2d8b6e465886673cf7e80e4afd5b2a6cced0
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-13 18:08:11 +0100

    Update Slideshow navigation functions

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -7,22 +7,43 @@ import {
 } from "@fortawesome/free-solid-svg-icons"
 
 function Slideshow({ images }) {
-  const firstImage = images[0]
-  const [currentImage, changeImage] = useState(firstImage)
+  const [currentSlide, changeSlide] = useState(0)
+
+  function nextSlide() {
+    const newSlide = currentSlide + 1
+    changeSlide(newSlide < images.length ? newSlide : 0)
+  }
+  function prevSlide() {
+    const newSlide = currentSlide - 1
+    changeSlide(newSlide >= 0 ? newSlide : images.length - 1)
+  }
+
   return (
     <div className="slideshow">
       {images.length > 1 && (
         <div className="slideshow__nav">
-          <div className="slideshow__arrow slideshow__arrow--left">
+          <div
+            className="slideshow__arrow slideshow__arrow--left"
+            onClick={prevSlide}
+          >
             <FontAwesomeIcon icon={faChevronLeft} />
           </div>
-          <div className="slideshow__pagination"></div>
-          <div className="slideshow__arrow slideshow__arrow--right">
+          <div className="slideshow__pagination">
+            {currentSlide + 1}/{images.length}
+          </div>
+          <div
+            className="slideshow__arrow slideshow__arrow--right"
+            onClick={nextSlide}
+          >
             <FontAwesomeIcon icon={faChevronRight} />
           </div>
         </div>
       )}
-      <img className="slideshow__image" src={currentImage} alt="logement"></img>
+      <img
+        className="slideshow__image"
+        src={images[currentSlide]}
+        alt="logement"
+      ></img>
     </div>
   )
 }
