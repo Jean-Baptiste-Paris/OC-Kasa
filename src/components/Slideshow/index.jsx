b151ce77a370757c54a9386c4b69b6b505aa3ca9
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-02-13 13:48:10 +0100

    Update Slideshow component Affichage de la première image Affichage conditionnel de la navigation Ajout des premiers styles

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,1 +1,30 @@
+import { useState } from "react"
 import "./styles.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons"
+
+function Slideshow({ images }) {
+  const firstImage = images[0]
+  const [currentImage, changeImage] = useState(firstImage)
+  return (
+    <div className="slideshow">
+      {images.length > 1 && (
+        <div className="slideshow__nav">
+          <div className="slideshow__arrow slideshow__arrow--left">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </div>
+          <div className="slideshow__pagination"></div>
+          <div className="slideshow__arrow slideshow__arrow--right">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </div>
+        </div>
+      )}
+      <img className="slideshow__image" src={currentImage} alt="logement"></img>
+    </div>
+  )
+}
+
+export default Slideshow
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -2,17 +2,19 @@ import { Navigate, useParams } from "react-router-dom"
 import logements from "../../assets/data/logements.json"
 import InfoLogement from "../../components/InfoLogement"
 import Collapse from "../../components/Collapse"
+import Slideshow from "../../components/Slideshow"
 
 function FicheLogement() {
   const { id } = useParams()
   const logement = logements.find((element) => element.id === id)
-  const { description, equipments } = logement
+  const { description, equipments, pictures } = logement
 
   if (!logement) {
     return <Navigate to="/404" replace={true} />
   }
   return (
     <div className="Logement page">
+      <Slideshow images={pictures} />
       <InfoLogement {...logement} className="Logement__infos" />
       <div className="Logement__collapses">
         <Collapse title="Description">
